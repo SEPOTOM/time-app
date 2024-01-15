c7985d42999129a5189bfa76817cc45201270fab
Artyom Pizin <artempetr2016@gmail.com>
2024-01-15 23:04:32 +0300

    refactor: add index file to the components folder

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as App } from './App/App';
+export { default as ClockFace } from './ClockFace/ClockFace';
+export { default as RoundButton } from './RoundButton/RoundButton';
+export { default as RoundedButton } from './RoundedButton/RoundedButton';
