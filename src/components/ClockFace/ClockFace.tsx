52201e8f20e1ac8345f8301cd93ea6a73ca09ec3
Artyom Pizin <artempetr2016@gmail.com>
2024-01-16 22:18:26 +0300

    feat: add validation to ClockFaceInput component

--- src/components/ClockFace/ClockFace.tsx
+++ src/components/ClockFace/ClockFace.tsx
@@ -1,10 +1,55 @@
 import './index.css';
 
-import { MouseEvent } from 'react';
+import {
+  MouseEvent,
+  KeyboardEvent,
+  ChangeEvent,
+  FocusEvent,
+  useState,
+} from 'react';
 
-const ClockFaceInput = () => {
+import { InputProps } from './types';
+
+const ClockFaceInput = ({ maxValue }: InputProps) => {
+  const [value, setValue] = useState('00');
+
   const handleClick = (e: MouseEvent<HTMLInputElement>) => {
     e.currentTarget.select();
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    const REGEXP = /^[0-9]*$/;
+
+    if (!REGEXP.test(e.key) && e.key !== 'Backspace') {
+      e.preventDefault();
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const currentValue = e.currentTarget.value;
+
+    if (currentValue.length > 2) {
+      return;
+    }
+
+    if (Number(currentValue) > maxValue) {
+      setValue(String(maxValue));
+      return;
+    }
+
+    setValue(currentValue);
+  };
+
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    if (e.currentTarget.value.length < 2) {
+      let formattedValue = e.currentTarget.value;
+
+      while (formattedValue.length < 2) {
+        formattedValue = `0${formattedValue}`;
+      }
+
+      setValue(formattedValue);
+    }
   };
 
   return (
@@ -12,8 +57,11 @@ const ClockFaceInput = () => {
       className="clock-face__input"
       type="number"
       inputMode="numeric"
-      defaultValue="00"
+      value={value}
       onClick={handleClick}
+      onKeyDown={handleKeyDown}
+      onChange={handleChange}
+      onBlur={handleBlur}
     />
   );
 };
@@ -21,11 +69,11 @@ const ClockFaceInput = () => {
 const ClockFace = () => {
   return (
     <div className="clock-face">
-      <ClockFaceInput />
+      <ClockFaceInput maxValue={99} />
       <span className="clock-face__separator">:</span>
-      <ClockFaceInput />
+      <ClockFaceInput maxValue={59} />
       <span className="clock-face__separator">:</span>
-      <ClockFaceInput />
+      <ClockFaceInput maxValue={59} />
     </div>
   );
 };
