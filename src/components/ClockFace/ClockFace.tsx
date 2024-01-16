ba63dcb0b6b61c216fb354532b4556af75663662
Artyom Pizin <artempetr2016@gmail.com>
2024-01-16 21:51:33 +0300

    feat: create ClockFaceInput component

--- src/components/ClockFace/ClockFace.tsx
+++ src/components/ClockFace/ClockFace.tsx
@@ -1,13 +1,31 @@
 import './index.css';
 
+import { MouseEvent } from 'react';
+
+const ClockFaceInput = () => {
+  const handleClick = (e: MouseEvent<HTMLInputElement>) => {
+    e.currentTarget.select();
+  };
+
+  return (
+    <input
+      className="clock-face__input"
+      type="number"
+      inputMode="numeric"
+      defaultValue="00"
+      onClick={handleClick}
+    />
+  );
+};
+
 const ClockFace = () => {
   return (
     <div className="clock-face">
-      <span className="clock-face__time">00</span>
+      <ClockFaceInput />
       <span className="clock-face__separator">:</span>
-      <span className="clock-face__time">00</span>
+      <ClockFaceInput />
       <span className="clock-face__separator">:</span>
-      <span className="clock-face__time">00</span>
+      <ClockFaceInput />
     </div>
   );
 };
