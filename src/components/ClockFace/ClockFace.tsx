510fe8258484b6a1e375f6b908759fc0af9d93bf
Artyom Pizin <artempetr2016@gmail.com>
2024-01-18 19:40:40 +0300

    feat: implement countdown functionality in ClockFace component

--- src/components/ClockFace/ClockFace.tsx
+++ src/components/ClockFace/ClockFace.tsx
@@ -6,13 +6,12 @@ import {
   ChangeEvent,
   FocusEvent,
   useState,
+  useEffect,
 } from 'react';
 
-import { InputProps } from './types';
+import { Props, InputProps, TimeState } from './types';
 
-const ClockFaceInput = ({ maxValue }: InputProps) => {
-  const [value, setValue] = useState('00');
-
+const ClockFaceInput = ({ maxValue, value, onChange }: InputProps) => {
   const handleClick = (e: MouseEvent<HTMLInputElement>) => {
     e.currentTarget.select();
   };
@@ -33,11 +32,11 @@ const ClockFaceInput = ({ maxValue }: InputProps) => {
     }
 
     if (Number(currentValue) > maxValue) {
-      setValue(String(maxValue));
+      onChange(String(maxValue));
       return;
     }
 
-    setValue(currentValue);
+    onChange(currentValue);
   };
 
   const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
@@ -48,7 +47,7 @@ const ClockFaceInput = ({ maxValue }: InputProps) => {
         formattedValue = `0${formattedValue}`;
       }
 
-      setValue(formattedValue);
+      onChange(formattedValue);
     }
   };
 
@@ -66,14 +65,134 @@ const ClockFaceInput = ({ maxValue }: InputProps) => {
   );
 };
 
-const ClockFace = () => {
+const ClockFace = ({ countdownStarted }: Props) => {
+  const [time, setTime] = useState<TimeState>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  });
+
+  useEffect(() => {
+    let timeoutId = 0;
+
+    const getNewSeconds = ({ seconds, minutes, hours }: TimeState): string => {
+      const newValue = Number(seconds) - 1;
+
+      if (newValue < 0 && hours === '00' && minutes === '00') {
+        return '00';
+      } else if (newValue < 0) {
+        return '59';
+      }
+
+      let formattedValue = String(newValue);
+
+      while (formattedValue.length < 2) {
+        formattedValue = `0${formattedValue}`;
+      }
+
+      return formattedValue;
+    };
+
+    const getNewMinutes = ({ seconds, minutes, hours }: TimeState): string => {
+      if (seconds !== '00') {
+        return minutes;
+      }
+
+      const newValue = Number(minutes) - 1;
+
+      if (newValue < 0 && hours === '00') {
+        return '00';
+      } else if (newValue < 0) {
+        return '59';
+      }
+
+      let formattedValue = String(newValue);
+
+      while (formattedValue.length < 2) {
+        formattedValue = `0${formattedValue}`;
+      }
+
+      return formattedValue;
+    };
+
+    const getNewHours = ({ seconds, minutes, hours }: TimeState): string => {
+      if (seconds !== '00' || minutes !== '00') {
+        return hours;
+      }
+
+      const newValue = Number(hours) - 1;
+
+      if (newValue < 0) {
+        return '00';
+      }
+
+      let formattedValue = String(newValue);
+
+      while (formattedValue.length < 2) {
+        formattedValue = `0${formattedValue}`;
+      }
+
+      return formattedValue;
+    };
+
+    const decreaseTime = () => {
+      setTime((t) => {
+        if (t.hours === '00' && t.minutes === '00' && t.seconds === '00') {
+          return t;
+        }
+
+        const newSeconds = getNewSeconds(t);
+        const newMinutes = getNewMinutes(t);
+        const newHours = getNewHours(t);
+
+        return {
+          hours: newHours,
+          minutes: newMinutes,
+          seconds: newSeconds,
+        };
+      });
+
+      timeoutId = setTimeout(decreaseTime, 1000);
+    };
+
+    if (countdownStarted) {
+      timeoutId = setTimeout(decreaseTime, 1000);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [countdownStarted]);
+
+  const handleHoursChange = (newValue: string) => {
+    setTime((t) => ({ ...t, hours: newValue }));
+  };
+
+  const handleMinutesChange = (newValue: string) => {
+    setTime((t) => ({ ...t, minutes: newValue }));
+  };
+
+  const handleSecondsChange = (newValue: string) => {
+    setTime((t) => ({ ...t, seconds: newValue }));
+  };
+
   return (
     <div className="clock-face">
-      <ClockFaceInput maxValue={99} />
+      <ClockFaceInput
+        maxValue={99}
+        value={time.hours}
+        onChange={handleHoursChange}
+      />
       <span className="clock-face__separator">:</span>
-      <ClockFaceInput maxValue={59} />
+      <ClockFaceInput
+        maxValue={59}
+        value={time.minutes}
+        onChange={handleMinutesChange}
+      />
       <span className="clock-face__separator">:</span>
-      <ClockFaceInput maxValue={59} />
+      <ClockFaceInput
+        maxValue={59}
+        value={time.seconds}
+        onChange={handleSecondsChange}
+      />
     </div>
   );
 };
--- src/components/ClockFace/types.ts
+++ src/components/ClockFace/types.ts
@@ -1,3 +1,15 @@
+export interface Props {
+  countdownStarted: boolean;
+}
+
 export interface InputProps {
   maxValue: number;
+  value: string;
+  onChange(newValue: string): void;
+}
+
+export interface TimeState {
+  hours: string;
+  minutes: string;
+  seconds: string;
 }
