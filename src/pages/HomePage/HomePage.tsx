41daa196e676eacef1fa32c0ecad9e90d18915ad
Artyom Pizin <artempetr2016@gmail.com>
2024-02-04 21:09:40 +0300

    feat: remove header and footer from HomePage component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,21 +2,16 @@ import './index.css';
 
 import { Link } from 'react-router-dom';
 
-import { Footer } from '../../components';
-
 const HomePage = () => {
   return (
-    <div className="homepage">
-      <header className="homepage__header">
-        <Link className="homepage__button" to="/timer">
-          TIMER
-        </Link>
-        <Link className="homepage__button" to="/stopwatch">
-          STOPWATCH
-        </Link>
-      </header>
-      <Footer />
-    </div>
+    <main className="homepage">
+      <Link className="homepage__button" to="/timer">
+        TIMER
+      </Link>
+      <Link className="homepage__button" to="/stopwatch">
+        STOPWATCH
+      </Link>
+    </main>
   );
 };
 
