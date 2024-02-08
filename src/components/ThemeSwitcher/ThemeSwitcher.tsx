9a7c0b72f27c991d307d40bcad5211c3138b849d
Artyom Pizin <artempetr2016@gmail.com>
2024-02-08 19:56:41 +0300

    feat: add dark theme to ThemeSwitcher component

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -2,15 +2,20 @@ import './index.css';
 
 import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
 
+import { useTheme } from '../../contexts/ThemeContext';
+
 import { Props } from './types';
 
 const ThemeSwitcher = ({ isActive, onClick }: Props) => {
+  const theme = useTheme();
+
+  const className = `theme-switcher theme-switcher_${theme}`;
   const dotClassName = `theme-switcher__dot${
     isActive ? ' theme-switcher__dot_active' : ''
   }`;
 
   return (
-    <button className="theme-switcher" type="button" onClick={onClick}>
+    <button className={className} type="button" onClick={onClick}>
       <SunIcon className="them-switcher__icon" width={20} height={20} />
       <MoonIcon className="them-switcher__icon" width={20} height={20} />
       <div className={dotClassName} />
