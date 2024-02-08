faf84fb770e25ced7ddb1a261868ac21c15f337a
Artyom Pizin <artempetr2016@gmail.com>
2024-02-08 19:56:17 +0300

    feat: add dark theme to HomeButton component

--- src/components/HomeButton/HomeButton.tsx
+++ src/components/HomeButton/HomeButton.tsx
@@ -2,11 +2,17 @@ import './index.css';
 
 import { Link } from 'react-router-dom';
 
+import { useTheme } from '../../contexts/ThemeContext';
+
 import { Props } from './types';
 
 const HomeButton = ({ children }: Props) => {
+  const theme = useTheme();
+
+  const className = `home-button home-button_${theme}`;
+
   return (
-    <Link className="home-button" to="/">
+    <Link className={className} to="/">
       {children}
     </Link>
   );
