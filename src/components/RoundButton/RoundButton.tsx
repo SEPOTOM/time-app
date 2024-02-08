983b2ae0b62d2ef268362457740653c7468badc1
Artyom Pizin <artempetr2016@gmail.com>
2024-02-08 23:21:34 +0300

    feat: add dark theme to RoundButton component

--- src/components/RoundButton/RoundButton.tsx
+++ src/components/RoundButton/RoundButton.tsx
@@ -1,10 +1,16 @@
 import './index.css';
 
+import { useTheme } from '../../contexts/ThemeContext';
+
 import { Props } from './types';
 
 const RoundButton = ({ children, onClick }: Props) => {
+  const theme = useTheme();
+
+  const className = `round-button round-button_${theme}`;
+
   return (
-    <button className="round-button" type="button" onClick={onClick}>
+    <button className={className} type="button" onClick={onClick}>
       {children}
     </button>
   );
