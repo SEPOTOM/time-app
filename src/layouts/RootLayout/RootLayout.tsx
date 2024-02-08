d2e655486862adb3a3b03cc786f4b467f713fa32
Artyom Pizin <artempetr2016@gmail.com>
2024-02-08 19:53:17 +0300

    feat: add ability to change themes to RootLayout component

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,19 +1,30 @@
 import { HomeIcon } from '@heroicons/react/24/solid';
-import { HomeButton } from '../../components';
+import { HomeButton, ThemeSwitcher } from '../../components';
 import './index.css';
 import { Outlet } from 'react-router-dom';
+import ThemeContext, { Themes } from '../../contexts/ThemeContext';
+import { useState } from 'react';
 
 const RootLayout = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const handleThemeToggle = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  const theme = isDarkTheme ? Themes.DARK : Themes.LIGHT;
+
   return (
-    <>
+    <ThemeContext.Provider value={theme}>
       <header className="header">
         <HomeButton>
           <HomeIcon width={24} />
         </HomeButton>
+        <ThemeSwitcher isActive={isDarkTheme} onClick={handleThemeToggle} />
       </header>
       <Outlet />
       <footer className="footer">Time App</footer>
-    </>
+    </ThemeContext.Provider>
   );
 };
 
