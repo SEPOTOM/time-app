ea5f929ad8868a553f49672cefcdb2370790c77d
Artyom Pizin <artempetr2016@gmail.com>
2024-02-21 19:06:07 +0300

    refactor: refactor ConsoleLists component

--- src/components/Console/ConsoleLists.tsx
+++ src/components/Console/ConsoleLists.tsx
@@ -4,15 +4,15 @@ import { ListsProps } from './types';
 
 const ConsoleLists = ({ lapsTimes }: ListsProps) => {
   return lapsTimes
-    .map((lapsTime, index) => {
-      if (lapsTime.length === 0) {
+    .map((lapTimes, index) => {
+      if (lapTimes.length === 0) {
         return null;
       }
 
       return (
         <ConsoleList
           lapsTimes={lapsTimes}
-          lapTimes={lapsTime}
+          lapTimes={lapTimes}
           index={index}
           key={`lap-${index}`}
         />
