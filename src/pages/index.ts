8f199bb98f5b57ab3eea49848da3c4ee76ae5861
Artyom Pizin <artempetr2016@gmail.com>
2024-03-03 20:52:48 +0300

    feat: create ErrorPage component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as TimerPage } from './TimerPage/TimerPage';
 export { default as StopwatchPage } from './StopwatchPage/StopwatchPage';
 export { default as HomePage } from './HomePage/HomePage';
 export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
+export { default as ErrorPage } from './ErrorPage/ErrorPage';
