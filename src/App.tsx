3aa032cc8f4dfdfeac26a4c0d91337906656d5bf
Artyom Pizin <artempetr2016@gmail.com>
2024-03-04 21:23:16 +0300

    feat: use ErrorPage component as errorElement for the root route

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         errorElement: <ErrorPage />,
